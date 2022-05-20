49f56b9dbc36cfd03e5940f5079691ee1937bfcb
Peter <polonipeto@gmail.com>
2022-05-20 18:40:44 +0200

    set up for other functions

--- datafetcher/urls.py
+++ datafetcher/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('authorize/', views.authorize_user),
     path('authorize/get_top', views.get_access),
     path('authorize/get_top/average', views.avegare_count),
+    path('authorize/get_top/sort_by_a', views.avegare_count),
+    path('authorize/get_top/sort_by_i', views.avegare_count),
+    path('authorize/get_top/sort_by_p', views.avegare_count),
 ]

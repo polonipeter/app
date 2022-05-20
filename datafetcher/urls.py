b530c970dd05477a90cb48abb03da9cd68f51cb5
Peter <polonipeto@gmail.com>
2022-05-20 19:34:03 +0200

    added url for sort

--- datafetcher/urls.py
+++ datafetcher/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('authorize/get_top/average', views.avegare_count),
     path('authorize/get_top/sort_by_a', views.sort_by_alph),
     path('authorize/get_top/sort_by_i', views.avegare_count),
-    path('authorize/get_top/sort_by_p', views.avegare_count),
+    path('authorize/get_top/sort_by_p', views.sort_by_pop),
 ]

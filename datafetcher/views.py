2d8366e857430868be1a1db4b8f194101e1b6163
Peter <polonipeto@gmail.com>
2022-05-20 14:41:24 +0200

    added exchange for access token and loading data from json

--- datafetcher/views.py
+++ datafetcher/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .constants import CLIENTID, CLIENTSECERET
-from requests import get
+from requests import  post, get
 from django.http import HttpResponseRedirect
+from .constants import CLIENTID, CLIENTSECERET
+from .tasks import get_top, parse, format_data
 
 def authorize_user(request):
     scope = 'user-top-read'
@@ -16,3 +17,21 @@ def authorize_user(request):
     user = get('https://accounts.spotify.com/authorize',params=data)
     redirecturl = user.url
     return HttpResponseRedirect(redirecturl)
+
+def get_access(request):
+    author_code =request.GET.get('code')
+    data = {
+      'grant_type': 'authorization_code',
+      'client_id': CLIENTID,
+      'client_secret': CLIENTSECERET,
+      'redirect_uri': 'http://localhost:8000/datafetcher/authorize/get_top',
+      'code': author_code,
+    }
+    res = post('https://accounts.spotify.com/api/token', data).json()
+    access_token = res.get('access_token')
+    ret = get_top(access_token)
+    data = ret.json()
+    ret = parse(data, 'name')
+    ret2 = parse(data, 'popularity')
+    ret = format_data(ret, ret2)
+    return render(request, "index.html", {"name": ret})

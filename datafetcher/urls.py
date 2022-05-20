d44c580d1ed255e2c88ff106f52646bcef6d0568
Peter <polonipeto@gmail.com>
2022-05-20 19:13:40 +0200

    added url of sorted table

--- datafetcher/urls.py
+++ datafetcher/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('authorize/', views.authorize_user),
     path('authorize/get_top', views.get_access),
     path('authorize/get_top/average', views.avegare_count),
-    path('authorize/get_top/sort_by_a', views.avegare_count),
+    path('authorize/get_top/sort_by_a', views.sort_by_alph),
     path('authorize/get_top/sort_by_i', views.avegare_count),
     path('authorize/get_top/sort_by_p', views.avegare_count),
 ]

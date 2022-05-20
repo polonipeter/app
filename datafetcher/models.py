6cb98f67181385534ef174b7d09795b23935373f
Peter <polonipeto@gmail.com>
2022-05-20 16:53:20 +0200

    added model to help store informations needed to work with data

--- datafetcher/models.py
+++ datafetcher/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class user(models.Model):
+    access_token = models.CharField(max_length=500)
+    name = models.CharField(max_length=1000)
+    popularity = models.CharField(max_length=1000)
+    interpret = models.CharField(max_length=1000)

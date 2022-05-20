fb9056daaa4edc2266794e1df405e876261c95a9
Peter <polonipeto@gmail.com>
2022-05-20 19:40:14 +0200

    added url for sorting by interpret

--- datafetcher/urls.py
+++ datafetcher/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('authorize/get_top', views.get_access),
     path('authorize/get_top/average', views.avegare_count),
     path('authorize/get_top/sort_by_a', views.sort_by_alph),
-    path('authorize/get_top/sort_by_i', views.avegare_count),
+    path('authorize/get_top/sort_by_i', views.sort_by_inter),
     path('authorize/get_top/sort_by_p', views.sort_by_pop),
 ]

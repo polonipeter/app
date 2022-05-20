90764cb2bd9bf236528fda4bc598d2688ea85142
Peter <polonipeto@gmail.com>
2022-05-20 12:54:05 +0200

    added authorization to datafetcher views

--- datafetcher/views.py
+++ datafetcher/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .constants import CLIENTID, CLIENTSECERET
+from requests import get
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+def authorize_user(request):
+    scope = 'user-top-read'
+    state = 'a'
+    data = {
+      'response_type': 'code',
+      'client_id': CLIENTID,
+      'scope': scope,
+      'redirect_uri': 'http://localhost:8000/datafetcher/authorize/get_top',
+      'state':state,
+    }
+    user = get('https://accounts.spotify.com/authorize',params=data)
+    redirecturl = user.url
+    return HttpResponseRedirect(redirecturl)
